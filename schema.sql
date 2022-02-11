0e29e496cac11d9e3b31788cf0819c53b08875b1
maria palacio <palamari@kean.edu>
2022-02-11 04:26:03 -0500

    queries

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE departments(
 	UNIQUE (dept_name)
 );
 
-CREATE TABLE Employees(
+CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE Employees(
 );
 
 CREATE TABLE dept_manager(
-dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -41,18 +41,23 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_emp;
 
